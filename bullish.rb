12c2f103c1329402e8ddb7e7c2d99ca2cdf089ac
Joseph M Spinelli <josephspinelli@yahoo.com>
2018-09-05 05:20:40 +0000

    automatically backed up by learn

---
+++ bullish.rb
@@ -0,0 +1,13 @@
+def name
+  print "What is your name?"
+  name = gets.chomp
+  if name == "Joe"
+		puts "Hey #{name}"
+  elsif name == "Adam"
+    puts "Hey bitch #{name}"
+  else
+    puts "No sirry"
+  end
+end
+
+puts name
